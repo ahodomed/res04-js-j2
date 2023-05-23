4628209fa09896e80ef036771b68444a4aea5eac
ahodomed <ahodomed@gmail.com>
2023-05-23 13:16:40 +0000

    suite fonction

--- exercices-fonctions/fonction.js
+++ exercices-fonctions/fonction.js
@@ -1,3 +1,79 @@
+function add(nb1, nb2)
+{
+    return nb1 + nb2;
+}
+
+function sub(nb1, nb2)
+{
+    return nb1 - nb2;
+}
+
+function mult(nb1, nb2)
+{
+    return nb1 ** nb2;
+}
+
+function div(nb1, nb2)
+{
+    return nb1 / nb2;
+}
+
+function mod(nb1, nb2)
+{
+    return nb1 % nb2;
+}
+
+function askUserOperation()
+{
+    return window.prompt("Entrez un opérateur");
+}
+
+function askUserFirstNumber()
+{
+    return window.prompt("Entrez un nombre");
+}
+
+function askUserSecondNumber()
+{
+    return window.prompt("Entrez un autre nombre");
+}
+
+function dispatchOperation(nb1, ope, nb2)
+{
+    if(ope === "+")
+  {
+      console.log(`${nb1} ${ope} ${nb2} = ${add(nb1, nb2)}`);
+  }
+  else if(ope === "-")
+  {
+      console.log(`${nb1} ${ope} ${nb2} = ${sub(nb1, nb2)}`);
+  }
+  else if(ope === "*")
+  {
+      console.log(`${nb1} ${ope} ${nb2} = ${mult(nb1, nb2)}`);
+  }
+  else if(ope === "/")
+  {
+      console.log(`${nb1} ${ope} ${nb2} = ${div(nb1, nb2)}`);
+  }
+  else if(ope === "%")
+  {
+      console.log(`${nb1} ${ope} ${nb2} = ${mod(nb1, nb2)}`);
+  }
+}
+
+function calculator()
+{
+    let nb1 = askUserFirstNumber();
+  let ope = askUserOperation();
+  let nb2 = askUserSecondNumber();
+  
+  dispatchOperation(parseInt(nb1), ope, parseInt(nb2));
+}
+
+calculator();
+
+
 // *****************************************************************************
 // Exercice1 addition de deux nombres
 // *****************************************************************************
@@ -47,24 +123,51 @@ console.log(soustraction);
 
 let multiplication=mult(mult1,mult2);
 console.log(multiplication);
+
 // *****************************************************************************
-// Exercice2
+// division de deux nombres
+// *****************************************************************************
+    let divs=0;
+    let div1=165;
+    let div2=7;
+    
+    function div(d1,d2){
+        divs=d1/d2;
+        return divs;
+    }
+
+    let division=div(div1,div2)
+    
+    console.log(division);
+
+
+
+// *****************************************************************************
+// modulo de deux nombres
 // *****************************************************************************
 
+    let modulo=0;
+    let mod1=48;
+    let mod2= 7;
+    
+    function mod(mo1,mo2){
+        modulo=mo1%mo2;
+        return modulo;
+        
+    }
+    
+    let modulo1=mod(mod1,mod2);
+    console.log(modulo1);
 
-// Créer une fonction mult qui prend deux nombres en paramètres, et en retourne le produit.
 
-// Créer une fonction div qui prend deux nombres en paramètres, et en retourne le quotient.
+console.log(add(21, 21)); // doit afficher 42
+console.log(sub(63, 21)); // doit afficher 42
+console.log(mult(21, 2)); // doit afficher 42
+console.log(div(84, 2)); // doit afficher 42
+console.log(mod(42, 2)); // doit afficher 0
 
-// Créer une fonction mod qui prend deux nombres en paramètres, et en retourne le reste.
 
-
-
-
-// function computeVat(prices, vat)
-// {
-//     for(price of prices)
-//     {
-//         price = price * vat;
-//     }
-// }
+function askUserOperation(){
+   let quyestion=prompt("quelle opération voulez-vous faire");
+    
+}
